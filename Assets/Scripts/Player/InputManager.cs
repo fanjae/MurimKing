6e127ffd2df76fa7ddc38a422eef781bdb46d293
FanJae <hjjang45@gmail.com>
2026-06-04 22:58:33 +0900

    feat : 캐릭터 입력 정보를 수집하는 Input Manager 구현
    - 캐릭터의 입력 정보를 수집하는 Input Manager를 구현했습니다.

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    // 이동 값
+    public static Vector2 MoveMent;
+
+    private InputAction moveAction;
+
+    private void Awake()
+    {
+        if (moveAction == null) moveAction = InputSystem.actions.FindAction("Move");
+    }
+    private void Update()
+    {
+        MoveMent = moveAction.ReadValue<Vector2>();
+    }
+}
