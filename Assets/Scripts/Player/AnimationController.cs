55e48a287dd1dd8b7f8feab65431067d0e52723c
FanJae <hjjang45@gmail.com>
2026-06-04 22:58:54 +0900

    feat : Animation 상태를 바꿔주는 Controller 구현
    - Animator로 상태를 바꿔주는 Controller를 구현했습니다.

---
+++ Assets/Scripts/Player/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator refAnimator;
+    private void OnEnable()
+    {
+        if (refAnimator == null)
+        {
+            refAnimator = GetComponentInParent<Animator>();
+        }
+    }
+    
+
+    // 상태 교체를 애니메이터에 알림.
+    public void OnStateChanged(PlayerStateManager.State newState)
+    {
+        if (refAnimator == null) return;
+
+        refAnimator.SetInteger("State", (int)newState);
+    }
+}
