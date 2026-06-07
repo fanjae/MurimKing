dc10d9501a1ef565d80a29235ffb7ecca56320d3
FanJae <hjjang45@gmail.com>
2026-06-07 23:31:59 +0900

    feat : State Manager에 전투모드 On/Off 전환 구현
    - State Manager에 전투모드 On/Off 를 추가했습니다.

--- Assets/Scripts/State/PlayerStateManager.cs
+++ Assets/Scripts/State/PlayerStateManager.cs
@@ -13,6 +13,10 @@ public class PlayerStateManager : MonoBehaviour
     private PlayerMovementController movementController;
     private AnimationController animationController;
 
+    [SerializeField] private bool isCombat = false;
+    [SerializeField] private float combatReturnTime = 5.0f;
+    private float combatTimer = 0.0f;
+
     private void Awake()
     {
         movementController = GetComponent<PlayerMovementController>();
@@ -28,6 +32,28 @@ public class PlayerStateManager : MonoBehaviour
     {
         /// 점프일때는 대기
         if (state == State.Jump) return;
+
+        // 전투 모드 On/Off
+        if (InputManager.IsCombat)
+        {
+            isCombat = !isCombat;
+            combatTimer = 0.0f;
+
+            animationController.SetCombat(isCombat);
+        }
+
+        if(isCombat)
+        {
+            combatTimer += Time.deltaTime;
+
+            if (combatTimer >= combatReturnTime) // 5초 지나면 전투 모드 해제
+            {
+                isCombat = false;
+                animationController.SetCombat(false);
+            }
+
+            return;
+        }
 
         // 지면에 있고, 점프 입력이 들어온 경우 처리
         if (movementController.IsGrounded && InputManager.IsJump && CanJumpCurrentState())
