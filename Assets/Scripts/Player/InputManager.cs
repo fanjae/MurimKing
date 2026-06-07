a02927606ec661cd13dd5f43a2599ecfc8d93867
FanJae <hjjang45@gmail.com>
2026-06-07 22:03:09 +0900

    feat : 점프 입력 추가
    - 점프에 대한 InputAction을 추가했습니다.

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -6,14 +6,23 @@ public class InputManager : MonoBehaviour
     // 이동 값
     public static Vector2 MoveMent;
 
+    // 점프 여부
+    public static bool IsJump { get; private set; } = false;
+
+
     private InputAction moveAction;
+
+    private InputAction jumpAction;
+
 
     private void Awake()
     {
-        if (moveAction == null) moveAction = InputSystem.actions.FindAction("Move");
+        if(moveAction == null) moveAction = InputSystem.actions.FindAction("Move");
+        if(jumpAction == null) jumpAction = InputSystem.actions.FindAction("Jump");
     }
     private void Update()
     {
         MoveMent = moveAction.ReadValue<Vector2>();
+        IsJump = jumpAction.WasPressedThisFrame();
     }
 }
