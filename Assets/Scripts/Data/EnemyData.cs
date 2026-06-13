facad80b87879c5fce20aacdf4359fe61e4e766d
FanJae <hjjang45@gmail.com>
2026-06-13 17:01:32 +0900

    Refactor : 적/아이템 Scriptable Object로 구조 변경
    - 적과 아이템을 Scriptable Object로 구조를 변경했습니다.

---
+++ Assets/Scripts/Data/EnemyData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public string enemyName;
+    public float maxHP = 30f;
+    public float attackDamage = 10f;
+    public float moveSpeed = 0f;
+}
---
+++ Assets/Scripts/Data/PotionData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Potion Data")]
+public class PotionData : ScriptableObject
+{
+    public float healAmount = 50.0f;
+    public AudioClip useSound;
+}
