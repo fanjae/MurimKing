6c8b753db20e1a73ae9c73ba1e4666605a77aca0
FanJae <hjjang45@gmail.com>
2026-06-13 17:16:49 +0900

    feat : 공격 판정 구현
    - 공격 판정을 구현했습니다.

---
+++ Assets/Scripts/Player/PlayerPunchHitBox.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunchHitbox : MonoBehaviour
+{
+    // 오른쪽 공격 판정 위치, 왼손 공격 판정 위치
+    [SerializeField] private Transform rightPunchPoint;
+    [SerializeField] private Transform leftPunchPoint;
+
+    [SerializeField] private float radius = 1f;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private float damage = 10f;
+
+    private Transform currentPunchPoint;
+
+    private bool isChecking;
+
+    // 이미 맞은 적에게 중복으로 데미지가 들어가지 않도록 맞은적 저장
+    private readonly HashSet<Collider> damagedTargets = new();
+
+    private void Update()
+    {
+        // 공격 중이 아니거나 판정 위치 없으면 무효
+        if (!isChecking || currentPunchPoint == null)
+            return;
+
+        Collider[] hits = Physics.OverlapSphere(
+            currentPunchPoint.position,
+            radius,
+            enemyLayer
+        );
+
+        foreach (Collider hit in hits)
+        {
+            // 이미 데미지를 준 대상 건너 뛰기
+            if (damagedTargets.Contains(hit))
+                continue;
+
+            damagedTargets.Add(hit);
+
+            // 부모 오브젝트에서 EnemyHealth 탐색
+            EnemyHealth enemy = hit.GetComponentInParent<EnemyHealth>();
+
+            if (enemy != null)
+            {
+                // 적에게 데미지 적용
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+
+    // 공격 시작 시 호출
+    public void AttackBegin(Transform punchPoint)
+    {
+        // 현재 공격에 사용할 판정 위치 지정
+        currentPunchPoint = punchPoint;
+        damagedTargets.Clear();
+        isChecking = true;
+    }
+
+    // 공격 종료 시 호출
+    public void AttackEnd()
+    {
+
+        // 판정 해제
+        isChecking = false;
+        currentPunchPoint = null;
+    }
+
+    // 공격 범위 표시
+    private void OnDrawGizmos()
+    {
+        if (rightPunchPoint != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(rightPunchPoint.position, radius);
+        }
+
+        if (leftPunchPoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(leftPunchPoint.position, radius);
+        }
+
+        if (currentPunchPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(currentPunchPoint.position, radius);
+        }
+    }
+}
--- Assets/Scripts/State/PlayerAttackState.cs
+++ Assets/Scripts/State/PlayerAttackState.cs
@@ -9,33 +9,46 @@ public class PlayerAttackState : PlayerStateBase
     [SerializeField] private UnityEvent OnAttackCheckEnd;
     //공격을 종료할때 발행할 이벤트
     [SerializeField] private UnityEvent OnAttackEnd;
+    [SerializeField] private PlayerPunchHitbox punchHitbox;
+
+    [SerializeField] private Transform rightPunchPoint;
+    [SerializeField] private Transform leftPunchPoint;
 
     //애니메이션 이벤트 리스너 메서드
-    private void AttackStart()
+    public void RightAttackStart()
     {
+        // 공격에 필요한 스태미나 사용 못하면 공격 x
         if (!manager.UseAttackStamina())
-        {
             return;
-        }
+
+        // 오른손 공격 판정 시작
+        if (punchHitbox != null)
+            punchHitbox.AttackBegin(rightPunchPoint);
 
         OnAttackBegin?.Invoke();
     }
-    //공격 시작 이벤트에 리스너 메서드를 등록할때 사용하는 메서드
-    public void SubscribeOnAttackBegin(UnityAction listener)
+    public void LeftAttackStart()
     {
-        OnAttackBegin?.AddListener(listener);
+        // 공격에 필요한 스태미나 사용 못하면 공격 x 
+        if (!manager.UseAttackStamina())
+            return;
+
+        // 왼손 공격 판정 시작
+        if (punchHitbox != null)
+            punchHitbox.AttackBegin(leftPunchPoint);
+
+        OnAttackBegin?.Invoke();
     }
+
+    
     //공격 애니메이션에서 AttackCheckEnd 이벤트가 발생할때 실행
     private void AttackCheckEnd()
     {
         //공격 판정 종료 이벤트
+        punchHitbox.AttackEnd();
         OnAttackCheckEnd?.Invoke();
     }
-    //공격 판정 종료 이벤트에 리스너 메서드를 등록 할때 사용하는 메서드
-    public void SubscribeOnAttackCheckEnd(UnityAction listener)
-    {
-        OnAttackCheckEnd?.AddListener(listener);
-    }
+  
     private void ComboCheck()
     {
         if (manager.IsNextAttackReserved)
@@ -48,9 +61,22 @@ public class PlayerAttackState : PlayerStateBase
     private void AttackEnd()
     {
         //공격 판정 종료 이벤트
+        punchHitbox.AttackEnd();
         OnAttackEnd?.Invoke();
         manager.ClearNextAttackReserved();
     }
+    //공격 시작 이벤트에 리스너 메서드를 등록할때 사용하는 메서드
+    public void SubscribeOnAttackBegin(UnityAction listener)
+    {
+        OnAttackBegin?.AddListener(listener);
+    }
+
+    //공격 판정 종료 이벤트에 리스너 메서드를 등록 할때 사용하는 메서드
+    public void SubscribeOnAttackCheckEnd(UnityAction listener)
+    {
+        OnAttackCheckEnd?.AddListener(listener);
+    }
+
     //공격 종료 이벤트에 리스너 메서드를 등록할때 사용하는 메서드
     public void SubscribeOnAttackEnd(UnityAction listener)
     {
