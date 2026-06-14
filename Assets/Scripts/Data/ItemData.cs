4d4f9c28461256d8779d01de283050b813c1ad55
FanJae <hjjang45@gmail.com>
2026-06-14 20:56:18 +0900

    feat : ItemData 구현
    - 아이템 Data를 구현했습니다.
    - 이전에 MyConsoleRPG에서 사용했던 형태를 유사하게 차용했습니다.

---
+++ Assets/Scripts/Data/ItemData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public enum ItemType
+{
+    Consumable,
+    Equipment,
+    Etc
+}
+
+public abstract class ItemData : ScriptableObject
+{
+    public int Id;
+    public string itemName;
+    [TextArea] public string description;
+    public Sprite icon;
+
+    public ItemType type;
+    public bool isStackable = true;
+    public int maxStackCount = 99;
+}
