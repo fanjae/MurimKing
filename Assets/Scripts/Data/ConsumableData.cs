764d52a59c789901d4371a829f16cf301fc1756f
FanJae <hjjang45@gmail.com>
2026-07-10 00:35:15 +0900

    feat : 소비아이템 개념 추가

---
+++ Assets/Scripts/Data/ConsumableData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Consumable")]
+public class ConsumableData : ItemData
+{
+    // 소비 아이템
+    // 타입(순수 회복)
+    // 회복 값
+
+    [Header("Consumable")]
+    [SerializeField] private ConsumableType consumableType;
+    [SerializeField] private int value;
+
+    public ConsumableType ConsuabmeType => consumableType;
+    public int Value => value;
+
+    public override bool Use(Player player)
+    {
+        if (player == null) return false;
+
+        switch (consumableType)
+        {
+            case ConsumableType.HealHp: // 체력 포션 사용
+                
+            default:
+                return false;
+        }
+    }
+}
