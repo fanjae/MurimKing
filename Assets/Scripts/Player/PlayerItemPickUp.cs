ea385b11fdcacc5d9b530addc302ea19ba9e377f
FanJae <hjjang45@gmail.com>
2026-07-10 22:43:27 +0900

    feat : 플레이어 인벤토리 수정 및 월드 아이템 습득 추가

---
+++ Assets/Scripts/Player/PlayerItemPickUp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerItemPickup : MonoBehaviour
+{
+    private Inventory inventory;
+
+    public void Init(Inventory inventory)
+    {
+        this.inventory = inventory;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WorldItem worldItem = other.GetComponent<WorldItem>();
+        if (worldItem == null) return;
+
+        worldItem.TryPickup(inventory);
+    }
+}
